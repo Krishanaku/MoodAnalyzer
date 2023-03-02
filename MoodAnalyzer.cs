981ab0d33d7cb1dcde062120d6d9eead846dab6d
Krishanaku <krishnakumar.kk650.kk@gmail.com>
2023-03-02 17:46:13 +0530

    Add : Given a Message, ability to analyse and respond Happy or Sad Mood

---
+++ MoodAnalyzer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyzer
+{
+    public  class MoodAnalys
+    {
+        
+        public string  CheckMoodAnalyzer() 
+        {
+            Console.WriteLine("Enter Happy or Sad to check through MoodAnalyzer");
+            string mood = Console.ReadLine();
+            if (mood == "Happy" || mood == "happy" ) 
+            {
+                return "Happy";
+                
+
+            }
+            else
+            {
+                return "Sad";
+                
+            }
+
+
+        }
+    }
+}
