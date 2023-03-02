0deef20ef98aaf97fc9ec0082f65e17a544e61ec
Krishanaku <krishnakumar.kk650.kk@gmail.com>
2023-03-02 17:43:29 +0530

    Add :  Mood analyzer Problem

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace MoodAnalyzer
+{
+    public class Program
+    {
+        public static void Main(string[] args) 
+        {
+            Console.WriteLine("Mood Analyzer Problem");
+        }
+
+    }
+}
