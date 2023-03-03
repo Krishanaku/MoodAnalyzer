c8af516ec0297723ed5ffe9997f6e85bac91eba7
Krishanaku <krishnakumar.kk650.kk@gmail.com>
2023-03-03 14:25:35 +0530

    Add : Handle Exception if User Provides Invalid Mood
    - Like NULL

--- Program.cs
+++ Program.cs
@@ -2,9 +2,35 @@
 {
     public class Program
     {
-        public static void Main(string[] args) 
+        public static void Main(string[] args)
         {
-          
+            Program.ErrorFun();
+        }
+
+        public static void ErrorFun()
+        {
+
+            Console.WriteLine("Enter the Mood Happy or Sad: ");
+            string mood = Console.ReadLine();
+            try
+            {
+                if (mood == "Happy")
+                {
+                    Console.WriteLine("Happy");
+                }
+                if (mood == "Sad")
+                {
+                    Console.WriteLine("Sad");
+                }
+                else
+                {
+                    throw new Exception();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
         }
 
     }
