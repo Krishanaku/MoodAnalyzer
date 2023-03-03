0673823bc5da027162e58fd681727ab177f250f7
Krishanaku <krishnakumar.kk650.kk@gmail.com>
2023-03-03 14:27:06 +0530

    Add : Handle Exception if User Provides Invalid Mood
    - Like NULL

--- MoodTest.cs
+++ MoodTest.cs
@@ -6,19 +6,18 @@ namespace TestProject1
     public class MoodTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void MoodAnalyser_Success()
         {
-            //Arrange
+            // Arrange
+            //MoodAnalyser m1 = new MoodAnalyser("happy");
+            MoodAnalyser m1 = new MoodAnalyser("Sad");
 
-            string s1 = "Happy";
-            MoodAnalys m1 = new MoodAnalys();
-            string expectedOutput = "Happy";
+            // Act
+            var result = m1.mood();
 
-            //Act
-            string actualOutput = m1.CheckMoodAnalyzer(s1);
+            // Assert
+            Assert.AreEqual("Sad", result);
 
-            //Assert
-            Assert.AreEqual(expectedOutput, actualOutput);
 
 
         }
