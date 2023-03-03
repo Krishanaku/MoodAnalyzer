6b7a2c90847669432478fa81a0a0e06890968da5
Krishanaku <krishnakumar.kk650.kk@gmail.com>
2023-03-03 15:25:06 +0530

    Add : Use Reflection to Create MoodAnalyser with Parameter Constructor - Use MoodAnalyserFactory to create MoodAnalyser Object with Message Parameneter

--- Program.cs
+++ Program.cs
@@ -7,17 +7,26 @@ namespace MoodAnalyzer
     {
         public static void Main(string[] args)
         {
-            /*    Console.WriteLine("Enter the Mood : ");
-                string mood = Console.ReadLine();
-                try
-                {
-                    MoodAnalyser m1 = new MoodAnalyser();
-                    m1.check(mood);
-                }
-                catch(Exception e) 
-                {
-                    Console.WriteLine(e.Message);
-                } */
+            Console.WriteLine("Enter the Mood : ");
+            string mood = Console.ReadLine();
+            MoodAnalyser m1 = new MoodAnalyser(mood);
+
+            Type t = typeof(MoodAnalyser);
+            ConstructorInfo[] info = t.GetConstructors(BindingFlags.Public | BindingFlags.Instance);
+            foreach (ConstructorInfo info2 in info)
+            {
+                Console.WriteLine(info2.Name);
+            }
+
+            /* try
+               {
+                   MoodAnalyser m1 = new MoodAnalyser();
+                   m1.check(mood);
+               }
+               catch(Exception e) 
+               {
+                   Console.WriteLine(e.Message);
+               } */
         }
 
 
