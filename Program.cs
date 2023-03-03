2fd8f0de914462cb7a6fa20e725decfc4ed7bfb0
Krishanaku <krishnakumar.kk650.kk@gmail.com>
2023-03-03 15:09:49 +0530

    Add : Given Class Name When Improper Should Throw MoodAnalysisException To pass this test case pass wrong class name catch Exception and throw Exception indicating No Such Class Error

--- Program.cs
+++ Program.cs
@@ -7,17 +7,17 @@ namespace MoodAnalyzer
     {
         public static void Main(string[] args)
         {
-            /*   MoodAnalyser m1 = new MoodAnalyser();
-               MoodAnalyser m2 = new MoodAnalyser("Happy");
-               if(m1.Equals(m2))
-               {
-                   Console.WriteLine("Both Objects Are Equal");
-               }
-               else 
-               {
-                   Console.WriteLine("Both Objects are Different");
-               }
-            */
+            /*    Console.WriteLine("Enter the Mood : ");
+                string mood = Console.ReadLine();
+                try
+                {
+                    MoodAnalyser m1 = new MoodAnalyser();
+                    m1.check(mood);
+                }
+                catch(Exception e) 
+                {
+                    Console.WriteLine(e.Message);
+                } */
         }
 
 
