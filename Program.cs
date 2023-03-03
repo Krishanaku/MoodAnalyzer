0e1fa035fd59a66dfdd02c6dc1560d4a3ed95613
Krishanaku <krishnakumar.kk650.kk@gmail.com>
2023-03-03 15:58:41 +0530

    Add : Given Happy Message Using Reflection When Proper Should Return HAPPY Mood To pass this TC use reflection to invoke analyseMood Method and show HAPPY mood

--- Program.cs
+++ Program.cs
@@ -7,10 +7,12 @@ namespace MoodAnalyzer
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Enter the Mood : ");
-            string mood = Console.ReadLine();
-            MoodAnalyser m1 = new MoodAnalyser();
-            m1.check(mood);
+            /*   Console.WriteLine("Enter the Mood : ");
+           string mood = Console.ReadLine();
+           MoodAnalyser m1 = new MoodAnalyser();
+           m1.check(mood);
+        */
+
             /*
                 Type t = typeof(MoodAnalyser);
                 ConstructorInfo[] info = t.GetConstructors(BindingFlags.Public|BindingFlags.Instance);
@@ -19,6 +21,8 @@ namespace MoodAnalyzer
                     Console.WriteLine(info2.Name);
                 } 
             */
+
+
 
         }
 
