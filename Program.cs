3a313659fec3fb0304a8428e22a8e5bd8b988fc7
Krishanaku <krishnakumar.kk650.kk@gmail.com>
2023-03-03 16:10:29 +0530

    Add : Given Happy Message When Improper Method Should Throw MoodAnalysisException To pass this Test Case pass wrong Method Name, catch the Exception and throw indicating No Such Method Error

--- Program.cs
+++ Program.cs
@@ -7,20 +7,17 @@ namespace MoodAnalyzer
     {
         public static void Main(string[] args)
         {
-            /*   Console.WriteLine("Enter the Mood : ");
-           string mood = Console.ReadLine();
-           MoodAnalyser m1 = new MoodAnalyser();
-           m1.check(mood);
-        */
-
-            /*
-                Type t = typeof(MoodAnalyser);
-                ConstructorInfo[] info = t.GetConstructors(BindingFlags.Public|BindingFlags.Instance);
-                foreach (ConstructorInfo info2 in info)
-                {
-                    Console.WriteLine(info2.Name);
-                } 
-            */
+            Console.WriteLine("Enter the mood : ");
+            string mood = Console.ReadLine();
+            try
+            {
+                MoodAnalyser m1 = new MoodAnalyser();
+                m1.check(mood);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
 
 
 
