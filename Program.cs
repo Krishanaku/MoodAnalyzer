385e11202391527dd2e3b2ef07a0b560529af007
Krishanaku <krishnakumar.kk650.kk@gmail.com>
2023-03-03 14:43:28 +0530

    Add : Given NULL Mood Should Throw MoodAnalysisException To pass this Test Case in try catch block throw MoodAnalysisException

--- Program.cs
+++ Program.cs
@@ -1,13 +1,23 @@
-﻿namespace MoodAnalyzer
+﻿using MoodAnalyserDemo;
+
+namespace MoodAnalyzer
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            //MoodAnalyser m1 = new MoodAnalyser();
-            //m1.ErrorFun();
+            Console.WriteLine("Enter the Mood : ");
+            string x = Console.ReadLine();
+            try
+            {
+                MoodAnalyser m1 = new MoodAnalyser();
+                m1.MoodException(x);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
-
 
 
     }
